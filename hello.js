6b32162690320840032a4f5ae76826a95e838f97
MaxLavo <MaxLavo@users.noreply.github.com>
2020-09-25 18:41:33 -0400

    Modification console.log();

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+console.log("Early trains once relied on gravity and rope for motion.");
+console.log(
+  "Dieu est mort ! Dieu reste mort ! Et c'est nous qui l'avons tué !"
+);
+console.log("Friedrich Nietzsche");
